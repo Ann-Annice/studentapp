bf683130433d28dbd0b0156b4422aebbf58827c3
Ann-Annice <annannice98@gmail.com>
2022-10-16 12:44:02 +0530

    student

---
+++ src/test/java/com/nestdigital/studentapp1/Studentapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.studentapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studentapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
